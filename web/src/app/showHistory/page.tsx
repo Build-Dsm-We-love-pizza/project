c94803c26412656f4acf75c31f5b0239af9d01eb
Peeyush Gupta <peeyush654@gmail.com>
2024-02-17 13:37:53 -0600

    added shadcn component

---
+++ web/src/app/showHistory/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function History() {
+  return (
+    <main>
+      <div>
+        <Button>Hello</Button>
+      </div>
+    </main>
+  );
+}
